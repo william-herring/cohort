95044e013d7c79d7c1c888f228e58fcc3c5cc4b2
William Herring <73984595+william-herring@users.noreply.github.com>
2023-08-06 17:21:09 +1000

    Auth setup

--- api/app.py
+++ api/app.py
@@ -1,16 +1,46 @@
-from flask import Flask, render_template, request, session
+import random
+from flask import Flask, request, abort, jsonify, url_for
 import os
-import requests
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
-from models import Person
+from models import User
 
-@app.route('/add-person', methods=['POST'])
-def index():
-    pass
+@app.route('/api/create-user', methods=['POST'])
+def create_user():
+    name = request.json.get('username')
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if name is None or password or email is None:
+        abort(400)
+    if User.query.filter_by(email=email).first() is not None:
+        abort(400)
+
+    surname = name.split()[1] if ' ' in name else name
+    user_id = (surname[:4] if len(surname) > 4 else surname) + str(random.randint(10, 99))
+    while User.query.filter_by(user_id=user_id).first() is not None:
+        user_id = (surname[:4] if len(surname) > 4 else surname) + str(random.randint(10, 99))
+
+    user = User(name=name, email=email, user_id=user_id)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+    token = create_access_token(identity=user.id)
+
+    return jsonify({ 'token': token }), 201
+
+@app.route('/api/get-user')
+@jwt_required
+def get_user():
+    user = User.query.get(get_jwt_identity())
+    return jsonify(user.__dict__) # lazy, change later to only include relevant data
--- api/models.py
+++ api/models.py
@@ -1,13 +1,22 @@
 from app import db
+from passlib.apps import custom_app_context as pwd_context
 
 
-class Person(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    password_hash = db.Column(db.String(128))
     name = db.Column(db.String(100), nullable=False)
-    user_id = db.Column(db.String(10), unique=True, nullable=True)
-    role = db.Column(db.String(20), nullable=False, default='Student')
-    avatar = db.Column(db.String(300), nullable=False)
+    email = db.Column(db.String(100), nullabe=False)
+    user_id = db.Column(db.String(10), unique=True)
+    role = db.Column(db.String(20), default='Student')
+    avatar = db.Column(db.String(300))
     school = db.Column(db.Integer, db.ForeignKey('school.id'))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
     def __repr__(self):
         return f'<Person {self.user_id}>'
